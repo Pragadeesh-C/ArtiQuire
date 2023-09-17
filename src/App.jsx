fb85048fb651c69b2cac639b88b6fa4c689178ed
l3g4cy-0 <legacyclashsp@gmail.com>
2023-09-17 22:32:42 +0530

    login done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Login from "./Screens/Login";
+import Table from "./components/Table";
+import Navbar from "./components/Navbar";
 const App = () => {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/asd" element={<Login />} />
+          <Route path="/" element={<Table />} />
+          <Route path="/nav" element={<Navbar />} />
         </Routes>
       </Router>
     </div>
